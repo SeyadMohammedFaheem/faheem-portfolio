75db4d84b0e7a725c3c2d2da9606dc6e983677af
SeyadMohammedFaheem <faheemseyadmd@gmail.com>
2024-02-05 23:55:30 +0530

    Video Update

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
     var finalNumber = 99; // Set your desired final number
     var animationDuration = 10000; // Animation duration in milliseconds
@@ -13,9 +15,18 @@ $(document).ready(function () {
 
         if (currentNumber >= finalNumber) {
             clearInterval(interval); // Stop the interval when the final number is reached
-            $('.preloader').fadeOut(); // Hide the preloader
+            $('.preloader').animate({ bottom: '1080px' })(SlowMo); // Hide the preloader
         }
-    }, 1); // Update every millisecond for smooth animation
+    }, 0.2); // Update every millisecond for smooth animation
 
     $('.preloader').fadeIn(); // Show the preloader initially
 });
+document.addEventListener('mousemove', function (e) {
+    var cursorVideo = document.getElementById('cursorVideo');
+    var posX = e.clientX;
+    cursorVideo.style.transition = '0.1s ease'; // Adjust the duration and easing as needed
+    cursorVideo.style.left = posX + 'px';
+});
+
+
+
